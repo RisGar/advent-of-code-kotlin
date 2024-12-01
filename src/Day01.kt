2326672eed2cd01bb5400ce925e572a4b27ae37f
Rishab Garg <me@rishab-garg.me>
2024-12-01 19:30:09 +0100

    style: 2 space width tabs

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,25 +1,25 @@
 import kotlin.math.abs
 
 fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
-    return input.foldRight(Pair(mutableListOf<Int>(), mutableListOf<Int>())) { s, acc ->
-        val (first, second) = s.split("   ")
-        acc.first.add(first.toInt())
-        acc.second.add(second.toInt())
-        acc
-    }
+  return input.foldRight(Pair(mutableListOf<Int>(), mutableListOf<Int>())) { s, acc ->
+    val (first, second) = s.split("   ")
+    acc.first.add(first.toInt())
+    acc.second.add(second.toInt())
+    acc
+  }
 }
 
 fun part1(input: Pair<List<Int>, List<Int>>): Int {
-    return input.first.sorted().zip(input.second.sorted()).sumOf { xs -> abs(xs.first - xs.second) }
+  return input.first.sorted().zip(input.second.sorted()).sumOf { xs -> abs(xs.first - xs.second) }
 }
 
 fun part2(input: Pair<List<Int>, List<Int>>): Int {
-    return input.first.sumOf { e -> e * input.second.count { f -> e == f } }
+  return input.first.sumOf { e -> e * input.second.count { f -> e == f } }
 }
 
 
 fun main() {
-    val input = parseInput(readInput("Day01"))
-    part1(input).println()
-    part2(input).println()
+  val input = parseInput(readInput("Day01"))
+  part1(input).println()
+  part2(input).println()
 }
